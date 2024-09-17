af09994c9ffba94b900e49e0a8d391d89a1fc171
Siphelele <sphllzulu@gmail.com>
2024-09-17 10:13:06 +0200

    added the home, about and api routes, used request methods to handle requests, used postman to test,if page is not found, you will be redirected to the not found page

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
-const http = require('http')
+
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("We are live baby!!!\n");
+
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home page");
+  } else if (req.url === "/about" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is the about page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        firstName: "Siphelele",
+        lastName: "Zulu",
+      })
+    );
+
+  } else if (req.url === "/post" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on('end', ()=>{
+      console.log(body)
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Data received", data: body }));
+    })
+
+
+  } else {
+    res.writeHead(404);
+    res.end("Page not found, ufunani");
+  }
 });
 
-const PORT = 3004;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
-
